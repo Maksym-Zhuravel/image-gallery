b7fb8718bf8fa47cfdf7c8b55a5c35d251c8b41c
Maksym-Zhuravel <allwegotizus1@gmail.com>
2024-07-12 20:12:39 +0300

    update project

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,32 +2,39 @@ import { useEffect, useState } from "react";
 import SearchBar from "../SearchBar/SearchBar";
 import { getImages } from "../images-api";
 import ImageGallery from "../ImageGallery/ImageGallery";
+import LoadMoreBtn from "../LoadMoreBtn/LoadMoreBtn";
 
 export default function App() {
   const [images, setImages] = useState([]);
   const [query, setQuery] = useState("");
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
     async function fetchImages() {
       try {
-        const data = await getImages(query);
-        setImages(data);
+        const data = await getImages(query, page);
+        setImages((prevImages) => [...prevImages, ...data]);
       } catch (err) {
         console.log(err);
       }
     }
 
     fetchImages();
-  }, [query]);
+  }, [query, page]);
 
   const handleSearch = async (values) => {
     setQuery(values);
+  };
+
+  const handleLoadMore = async () => {
+    setPage(page + 1);
   };
 
   return (
     <>
       <SearchBar onSearch={handleSearch} />
       <ImageGallery images={images} />
+      <LoadMoreBtn onClick={handleLoadMore} />
     </>
   );
 }
--- src/components/images-api.js
+++ src/components/images-api.js
@@ -3,11 +3,12 @@ import axios from "axios";
 axios.defaults.baseURL = "https://api.unsplash.com/";
 const ACCESS_KEY = "QLGrhpFuPQS90snYgs05KNQ37GlsBKcdPZS8FDietBg";
 
-export const getImages = async (topic) => {
+export const getImages = async (topic, currentPage) => {
   const response = await axios.get("/search/photos", {
     params: {
       client_id: ACCESS_KEY,
       query: topic,
+      page: currentPage,
     },
   });
   return response.data.results;
